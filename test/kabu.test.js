a467e8b4f4b337a41931b2b0bfc3d966191ef30a
Hidehiko Naito <hidehiko@7110.email>
2018-11-12 01:25:55 +0900

    updupdate kabu.test.js to add search method test

--- test/kabu.test.js
+++ test/kabu.test.js
@@ -35,4 +35,35 @@ describe("Kabu", () => {
       expect(res).toEqual(yahoo);
     });
   });
+
+  // test of search method
+  describe("search", () => {
+    let sb1 = {
+      code: 4726,
+      name: "ソフトバンク・テクノロジー",
+      market: "市場第一部（内国株）",
+      industry17: "情報通信・サービスその他 ",
+      industry33: "情報・通信業"
+    };
+
+    let sb2 = {
+      code: 9984,
+      name: "ソフトバンクグループ",
+      market: "市場第一部（内国株）",
+      industry17: "情報通信・サービスその他 ",
+      industry33: "情報・通信業"
+    };
+
+    test("not match", () => {
+      let res = kabu.search("7110");
+      expect(res).toEqual([]);
+    });
+
+    test("match ソフトバンク", () => {
+      let res = kabu.search("ソフトバンク");
+      expect(res.length).toEqual(2);
+      expect(res[0]).toEqual(sb1);
+      expect(res[1]).toEqual(sb2);
+    });
+  });
 });
