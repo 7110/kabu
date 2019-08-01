8906336d9388097394cdd036cdec7fa3acd7eeb0
Hidehiko Naito <hidehiko@7110.email>
2019-08-02 01:36:31 +0900

    update kabu.js

--- src/kabu.js
+++ src/kabu.js
@@ -1,64 +1,64 @@
-"use strict";
+'use strict';
 
 export class Kabu {
   constructor() {
-    this.__main = require("./data/main.json");
-    this.__manual = require("./data/manual.json");
+    this._main = require('./data/main.json');
+    this._manual = require('./data/manual.json');
 
-    this.__author = "7110";
-    this.version = "1.0.7";
-    this.updated = "2019/07";
+    this._author = '7110';
+    this.version = '1.0.8';
+    this.updated = '2019/07';
   }
 
-  __substitute(item) {
+  _substitute(item) {
     const res = {};
     if (item && (item.c && item.n)) {
-      res["code"] = item.c;
-      res["name"] = item.n;
-      res["market"] = this.__manual["markets"][item.m];
-      res["industry17"] = this.__manual["industry17"][item.i17];
-      res["industry33"] = this.__manual["industry33"][item.i33];
+      res['code'] = item.c;
+      res['name'] = item.n;
+      res['market'] = this._manual['markets'][item.m];
+      res['industry17'] = this._manual['industry17'][item.i17];
+      res['industry33'] = this._manual['industry33'][item.i33];
     }
     return res;
   }
 
-  __getIndustry(num) {
+  _getIndustry(num) {
     const res = [];
-    for (let k in this.__manual[`industry${num}`]) {
-      res.push(this.__manual[`industry${num}`][k]);
+    for (let k in this._manual[`industry${num}`]) {
+      res.push(this._manual[`industry${num}`][k]);
     }
     return res;
   }
 
   // get: return brand name details searched by code
   get(code) {
-    const res = this.__main.find(item => {
+    const res = this._.find(item => {
       return item.c == code;
     });
-    return this.__substitute(res);
+    return this._substitute(res);
   }
 
   // search: return list of brand name details searched by keyword
   search(keyword) {
-    const res = this.__main.filter(item => {
+    const res = this._main.filter(item => {
       return item.n.includes(keyword);
     });
 
     const results = [];
     for (let i = 0; i < res.length; i++) {
-      results.push(this.__substitute(res[i]));
+      results.push(this._substitute(res[i]));
     }
     return results;
   }
 
   // getIndustry17List: return list of 17 Industry classification
   getIndustry17List() {
-    return this.__getIndustry("17");
+    return this._getIndustry('17');
   }
 
   // getIndustry33List: return list of 33 Industry classification
   getIndustry33List() {
-    return this.__getIndustry("33");
+    return this._getIndustry('33');
   }
 
   // help: print document URL, version, updated date and author
